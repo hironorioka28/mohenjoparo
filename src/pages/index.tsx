d5aea54dc7072e0d3c7f372a3cb77dd898f4e9b4
Hironori Oka <hironorioka28@gmail.com>
2022-01-31 00:22:22 +0900

    setup tsconfig, seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
 import {
   Heading,
 } from '@chakra-ui/react'
+import { DefaultSeo } from 'next-seo'
+import Head from 'next/head'
 import { DarkModeSwitch } from '../components/DarkModeSwitch'
+import config from '@config/seo.json'
 
 const Index = () => {
   return (
     <>
+      <DefaultSeo {...config} />
+      <Head>
+
+      </Head>
       <Heading as="h1">Hello Mohenjo Paro</Heading>
       <DarkModeSwitch />
     </>
